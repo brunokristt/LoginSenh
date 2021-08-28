e934dc3ad52be4dc1be2f2ded44ee13796d6a3b3
brunokristt <brunokristt@gmail.com>
2021-08-28 18:25:22 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace SenhaLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua senha para login");
+            string digitado = Console.ReadLine();
+            string senha = digitado.ToUpper(new CultureInfo("br-BR", false));
+
+            while (senha != "AMOR")
+            {
+                Console.WriteLine("Senha incorreta!");
+                Console.WriteLine("Digite sua senha para login");
+                digitado = Console.ReadLine();
+                senha = digitado.ToUpper(new CultureInfo("br-BR", false));
+            }
+            Console.WriteLine("Acesso Autorizado");
+        }
+    }
+}
+
